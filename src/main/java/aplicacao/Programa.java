b656b71a5ab77a167638f46d829ecaa6436a6340
leonardo <leonardomotamaier@gmail.com>
2022-07-05 10:13:15 -0300

    sonner dia 2

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,30 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+
+        EntityManager em = emf.createEntityManager();
+
+
+        Pessoa p = em.find(Pessoa.class, 2);
+
+        em.getTransaction().begin();
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+
+
+        System.out.println(p);
+
+
+    }
+}
